2766c0393b14252a5778fcaabe96f3fef249eff2
Tejas Birje <tejasbirje30@gmail.com>
2024-06-19 20:08:15 +0530

    add user and video models

---
+++ src/models/user.model.js
@@ -0,0 +1,95 @@
+import mongoose from "mongoose";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt"
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true,
+        index: true  // To make any field searchable, make index: true, without it we can still seacrch but this is more optimized but affects performance
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true,
+    },
+    fullName: {
+        type: String,
+        required: true,
+        trim: true,
+        index: true
+    },
+    avatar: {
+        type: String, //storing cloudinary URL
+        required: true,
+    },
+    coverImage: {
+        type: String,  //storing cloudinary URL
+        watchHistory : [ // storing IDs of videos watchted,
+            {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "Video"
+            }
+        ]
+    },
+    password: {
+        type: String,
+        required: [true, 'Password is required']
+    },
+    refreshToken: {
+        type: String,
+    }   
+
+}, {timestamps: true})
+
+// .pre()  --> do this right before event of save db. We also have .post for after event.
+// DO NOT use arrow functions () => {} as we cant use 'this' keyword to get current context. So use function () {} in the callback. Also make it async as it takes time to execute.
+
+userSchema.pre("save", async function () {
+    
+    // Only encrypt password if the password is changed. So if user changes avatar or anything other than password, don't encrypt password.
+    if(!this.isModified("password")) return next();
+
+    this.password = bcrypt.hash(this.password, 10)
+    next()
+})
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+   return await bcrypt.compare(password, this.password)
+}
+
+userSchema.methods.generateAccessToken = function() {
+    return jwt.sign(
+        {
+            _id: this._id,
+            email: this.email,
+            username: this.username,
+            fullName: this.fullName
+            // name of payload: coming from database
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+
+userSchema.methods.generateRefreshToken = function() {
+    return jwt.sign(
+        {
+            _id: this._id,
+            // name of payload: coming from database
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
+
+export const User = mongoose.model("User", userSchema);
--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -6,12 +6,7 @@ const asyncHandler = (requestHandler) => {
 
 
 
-export {asyncHandler}
-
-
-
-
-
+export { asyncHandler }
 
 
 
